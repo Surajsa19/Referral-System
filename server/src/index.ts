901050e7ba3f04b6b63abc2f56aa37dd1181750c
SurajSa-19 <surajsa19092003@gmail.com>
2025-12-09 22:23:20 +0530

    Update CORS to allow Vercel frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,10 @@ connectDB();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://filesure-suraj.vercel.app"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
